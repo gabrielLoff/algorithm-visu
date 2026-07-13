6bd80aadfe7010bc088e35df3d52857db73c55af
gabrielLoff <gabiloff7@gmail.com>
2026-07-13 14:52:43 +0200

    refactor: unify direction constants (#9) (#16)

--- src/maze/prim.ts
+++ src/maze/prim.ts
@@ -1,13 +1,6 @@
 import { GridModel, CellType } from '../types';
-import { cellKey } from '../grid/gridUtils';
+import { cellKey, getNeighbors } from '../grid/gridUtils';
 import { ensureSolvable } from './mazeUtils';
-
-const DIRS: [number, number][] = [
-  [-2, 0],
-  [2, 0],
-  [0, -2],
-  [0, 2],
-];
 
 export function prim(grid: GridModel): GridModel {
   const cells = grid.cells.map((row) => [...row]);
@@ -21,20 +14,16 @@ export function prim(grid: GridModel): GridModel {
 
   const frontier: [number, number, number, number][] = [];
 
-  function addFrontier(r: number, c: number) {
-    for (const [dr, dc] of DIRS) {
-      const nr = r + dr;
-      const nc = c + dc;
-      if (nr <= 0 || nr >= rows - 1 || nc <= 0 || nc >= cols - 1) continue;
-      const nKey = cellKey({ row: nr, col: nc });
-      if (passage.has(nKey)) continue;
-      const wr = r + dr / 2;
-      const wc = c + dc / 2;
-      frontier.push([nr, nc, wr, wc]);
-    }
+  for (const n of getNeighbors(grid, { row: startR, col: startC }, 2)) {
+    const nr = n.pos.row;
+    const nc = n.pos.col;
+    if (nr <= 0 || nr >= rows - 1 || nc <= 0 || nc >= cols - 1) continue;
+    const nKey = cellKey({ row: nr, col: nc });
+    if (passage.has(nKey)) continue;
+    const wr = startR + (nr - startR) / 2;
+    const wc = startC + (nc - startC) / 2;
+    frontier.push([nr, nc, wr, wc]);
   }
-
-  addFrontier(startR, startC);
 
   while (frontier.length > 0) {
     const idx = Math.floor(Math.random() * frontier.length);
@@ -47,7 +36,16 @@ export function prim(grid: GridModel): GridModel {
     passage.add(cKey);
     passage.add(cellKey({ row: wr, col: wc }));
 
-    addFrontier(cr, cc);
+    for (const n of getNeighbors(grid, { row: cr, col: cc }, 2)) {
+      const nr = n.pos.row;
+      const nc = n.pos.col;
+      if (nr <= 0 || nr >= rows - 1 || nc <= 0 || nc >= cols - 1) continue;
+      const nKey = cellKey({ row: nr, col: nc });
+      if (passage.has(nKey)) continue;
+      const wr2 = cr + (nr - cr) / 2;
+      const wc2 = cc + (nc - cc) / 2;
+      frontier.push([nr, nc, wr2, wc2]);
+    }
   }
 
   for (let r = 0; r < rows; r++) {
